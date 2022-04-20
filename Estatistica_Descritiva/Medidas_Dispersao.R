5a1b5069c42cf74963ba466a0891fa1f7bd3297d
Faustajoaquim <fausta.j@ufabc.edu.br>
2022-04-20 01:05:01 -0300

    Update Medidas_Dispersao.R

--- Estatistica_Descritiva/Medidas_Dispersao.R
+++ Estatistica_Descritiva/Medidas_Dispersao.R
@@ -3,5 +3,27 @@ library(ggplot2)
 
 # Carregando o dataset
 Vendas<-read.csv("Vendas.csv",dec = ",",fileEncoding ="windows-1252") 
-usuarios<-read.table("Usuarios.csv", dec = ".",sep =",
-                ", h=T,fileEncoding ="windows-1252")
+
+
+                ###########  Medida de posicao ###########
+#######################################################################
+
+# Resumo dos dados
+head(vendas)
+tail(vendas)
+view(vendas)
+str(vendas)
+summary(vendas$Valor)
+summary(vendas$Custo)
+
+# Calculo da Media 
+Vendas$Valor<-as.numeric(Vendas$Valor) 
+mediaValor<-mean(Vendas$Valor)
+mediaVcusto<-mean(vendas$Custo)
+
+# Calculo da Media Ponderada
+weighted.mean(vendas$Valor, w = vendas$Custo)
+
+# Calculo da Mediana
+mediana<-median(Vendas$Valor) # calculo de mediana
+
