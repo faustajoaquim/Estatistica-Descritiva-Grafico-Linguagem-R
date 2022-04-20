716667b7ed4375e975880a4ab5a89b35b40885a8
Faustajoaquim <fausta.j@ufabc.edu.br>
2022-04-20 02:37:54 -0300

    Update Graficos.R

--- Graficos/Graficos.R
+++ Graficos/Graficos.R
@@ -55,5 +55,53 @@ dev.off()
 ########### Pie Chart (grafico de pizza)#####################
 
 # criacao do vetor fatias
-
 fatias <- c(4, 12, 14, 16, 8)
+
+#vetor de paises
+paises <- c("Brasil", "Estados Unidos", "Alemanha", "Reino Unido", "Espanha")
+
+# grafico de pizza
+pie(fatias, labels = paises, main = "Leitura de Livros Por Pessoa/Ano")
+
+########### Pie Chart 3D (grafico de pizza 3D) #####################
+
+# criacao de vetor
+fatias <- c(4, 12, 14, 16, 8)
+
+#vetor de paises
+paises <- c("Brasil", "Estados Unidos", "Alemanha", "Reino Unido", "Espanha")
+
+# grafico de pizza 3d 
+pie3D(fatias, labels = paises, explode = 0.1, main = "Leitura de Livros Por Pessoa/Ano")
+
+###########  Line Chart (grafico de linha) #####################
+
+# criacao de vetor carros e caminhoes 
+carros <- c(1, 3, 6, 4, 9)
+caminhoes <- c(2, 5, 4, 5, 12) 
+
+# grafico line chart
+
+plot(carros, type = "o", col = "blue", ylim = c(0,12))
+lines(caminhoes, type = "o", pch = 22, lty = 2, col = "red")
+title(main = "ProduÃ§Ã£o de VeÃ­culos", col.main = "red", font.main = 4)
+
+########### Boxplot #####################
+
+# conjunto de dados do R (mpg)
+View(mpg)
+
+# grafico Boxplot
+ggplot(mpg, aes(x = reorder(class, hwy), y = hwy, fill = class)) + 
+  geom_boxplot() +
+  xlab("class") +
+  theme(legend.position = "none")
+
+ ###########  Scatter Plot #####################
+ 
+  ### Criando do data.frame data
+ data = data.frame(cond = rep(c("condition_1", "condition_2"), each=10), 
+                  my_x = 1:100 + rnorm(100,sd=9), my_y = 1:100 + rnorm(100,sd=16))
+
+View(data)
+
